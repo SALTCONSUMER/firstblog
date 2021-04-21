219925f162e9f2f8f1e48ed2b73a5b6d24c4ff59
KrpDnl <dkarpovich2000@gmail.com>
2021-04-22 02:56:37 +0300

    Creating model Post,patterns Index,Detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+
+
+
 def posts_list(request):
-    n=['asdasd','karp','Sasha']
-    return render(request,'blog/index.html',context={'names':n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request,slug):
+    post=Post.objects.get(slug__iexact=slug)
+    return render(request,'blog/post_detail.html',context={'post':post})
